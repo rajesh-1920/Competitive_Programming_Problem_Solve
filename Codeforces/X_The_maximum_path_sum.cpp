9af7a649a6d47322ee064fd3d33052844bf6bf08
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2024-04-25 23:17:40 +0600

    '25-04-2024'

--- Codeforces/X_The_maximum_path_sum.cpp
+++ Codeforces/X_The_maximum_path_sum.cpp
@@ -6,7 +6,7 @@ using namespace std;
 typedef long long int ll;
 typedef unsigned long long int ull;
 //----------------------------(definition section)-------------------------------------
-#define N 2000009
+#define N 200
 #define MOD 1000000009
 #define fi first
 #define sc second
@@ -21,8 +21,23 @@ typedef unsigned long long int ull;
 #define nl cout << "\n"
 #define rrr return
 //------------------------------------------------------------------------------------
+ll a[N][N],n,m;
+ll res(ll i,ll j)
+{
+    if(i>=n||j>=m)
+    return 0;
+    ll t1=a[i][j]+res(i+1,j);
+    ll t2=a[i][j]+res(i,j+1);
+    return max(t1,t2);
+}
 void solve()
 {
+    ll i,j;
+    cin>>n>>m;
+    for(i=0;i<n;i++)
+    for(j=0;j<m;j++)
+    cin>>a[i][j];
+    cout<<res(0,0);
 }
 //------------------------------------------------------------------------------------
 int main()
