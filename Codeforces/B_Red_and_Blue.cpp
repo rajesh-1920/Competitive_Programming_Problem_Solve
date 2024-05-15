9d4184610ec20ec6d7d539ed41ab874256c50461
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2024-05-15 17:27:54 +0600

    '15-05-2024'

--- Codeforces/B_Red_and_Blue.cpp
+++ Codeforces/B_Red_and_Blue.cpp
@@ -1,5 +1,5 @@
 // Author:  Rajesh Biswas
-// Date  :  15.05.2024
+// Date  :  14.05.2024
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -23,6 +23,43 @@ typedef unsigned long long int ull;
 //------------------------------------------------------------------------------------
 void solve()
 {
+    int n, x, i, j;
+    cin >> n;
+    vector<int> a, b;
+    int r[205];
+    for (int i = 0; i < n; i++)
+        cin >> x, a.push_back(x);
+    cin >> n;
+    for (i = 0; i < n; i++)
+        cin >> x, b.push_back(x);
+    int ans = 0, sa = 0, sb = 0, mxa = 0, mxb = 0, fl = 1;
+    i = j = 0;
+    while (true)
+    {
+        if (i == a.size() && j == b.size())
+            break;
+        if (fl)
+        {
+            if (i < a.size())
+            {
+                sa += a[i++];
+                mxa = max(sa, mxa);
+            }
+            if(j<b.size())fl=0;
+        }
+        else
+        {
+            if (j < b.size())
+            {
+                sb += b[j++];
+                mxb = max(sb, mxb);
+            }
+            if(i<a.size())fl=1;
+        }
+        ans=max(ans,(mxa+mxb));
+    }
+    cout << ans;
+    nl;
 }
 //------------------------------------------------------------------------------------
 int main()
@@ -31,7 +68,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int test = 1, T;
-    // cin >> test;
+    cin >> test;
     for (T = 1; T <= test; T++)
     {
         // cout << "Case " << T << ": ";
