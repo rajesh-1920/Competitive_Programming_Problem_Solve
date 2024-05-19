95cfb7838fd93a9c0b655636b92debf20727497c
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2024-05-19 22:54:15 +0600

    '19-05-2024'

--- V_judge/B_Ancient_Civilization.cpp
+++ V_judge/B_Ancient_Civilization.cpp
@@ -6,13 +6,13 @@ using namespace std;
 typedef long long int ll;
 typedef unsigned long long int ull;
 //----------------------------(definition section)-------------------------------------
-#define N 2000009
+#define N 2009
 #define MOD 1000000007
 #define fi first
 #define sc second
 #define pb(x) push_back(x)
 #define ppb(x, y) push_back({x, y})
- 
+
 #define No cout << "No\n"
 #define Yes cout << "Yes\n"
 #define YES cout << "YES\n"
@@ -21,8 +21,41 @@ typedef unsigned long long int ull;
 #define nl cout << "\n"
 #define rrr return
 //------------------------------------------------------------------------------------
+int c1[32], c0[32];
+int l;
+void bin_rep(int x)
+{
+    int i = 0;
+    for (i = 0; i < l; i++)
+    {
+        if (x & 1)
+            c1[i]++;
+        else
+            c0[i]++;
+        if (x > 0)
+            x >>= 1;
+    }
+}
 void solve(void)
 {
+    for (int i = 0; i < 32; i++)
+        c1[i] = c0[i] = 0;
+    int n;
+    cin >> n >> l;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        bin_rep(x);
+    }
+    int res = 0;
+    for (int i = 0; i < l; i++)
+    {
+        if (c1[i] > c0[i])
+            res |= (1 << i);
+    }
+    cout << res;
+    nl;
 }
 //------------------------------------------------------------------------------------
 int main()
@@ -31,7 +64,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int test = 1, T;
-    // cin >> test;
+    cin >> test;
     for (T = 1; T <= test; T++)
     {
         // cout << "Case " << T << ": ";
--- V_judge/R_MEXor_Mixup.cpp
+++ V_judge/R_MEXor_Mixup.cpp
@@ -6,13 +6,13 @@ using namespace std;
 typedef long long int ll;
 typedef unsigned long long int ull;
 //----------------------------(definition section)-------------------------------------
-#define N 2000009
+#define N 400009
 #define MOD 1000000007
 #define fi first
 #define sc second
 #define pb(x) push_back(x)
 #define ppb(x, y) push_back({x, y})
- 
+
 #define No cout << "No\n"
 #define Yes cout << "Yes\n"
 #define YES cout << "YES\n"
@@ -21,17 +21,27 @@ typedef unsigned long long int ull;
 #define nl cout << "\n"
 #define rrr return
 //------------------------------------------------------------------------------------
+map<int, int> mp;
 void solve(void)
 {
+    int a, b;
+    cin >> a >> b;
+    
 }
 //------------------------------------------------------------------------------------
 int main()
 {
+    int res = 0;
+    for (int i = 0; i <= N; i++)
+    {
+        res ^= i;
+        mp[i] = res;
+    }
     // cout << fixed << showpoint << setprecision(0);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int test = 1, T;
-    // cin >> test;
+    cin >> test;
     for (T = 1; T <= test; T++)
     {
         // cout << "Case " << T << ": ";
