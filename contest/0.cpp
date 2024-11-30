ad84c8aee1f3c4f215d4a3004956b2acc765548a
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2024-11-30 11:39:24 +0600

    template  done

--- contest/0.cpp
+++ contest/0.cpp
@@ -24,7 +24,7 @@ typedef long long int ll;
 #define MOD 1000000007
 #define N 200009
 //------------------------------------------------------------------------------------------
-/*/----------------------------(Order_set)--------------------------------------------------
+//----------------------------(Order_set)--------------------------------------------------
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
@@ -34,7 +34,7 @@ o_set<ll> st;
 ll pos = st.order_of_key(5);
 ll val = *st.find_by_order(2);
 //---------------------------------------------------------------------------------------*/
-/*/------------------------(Modular-Arithmatic)---------------------------------------------
+//------------------------(Modular-Arithmatic)---------------------------------------------
 inline ll _normal(ll A, ll M)
 {
     A = A - (A / M) * M;
@@ -73,7 +73,7 @@ inline ll moddiv(ll A, ll B, ll M)
 {
     A = _normal(A, M), B = _normal(B, M);
     return _normal(modmul(A, (binexpo(B, M - 2, M)), M), M);
-}/*
+}
 ll outer = 1;
 if (is_odd)
 {
@@ -95,7 +95,7 @@ else
         pod = modmul(pod, (binexpo(prime[i], _normal(powe[i] / 2 * outer, (MOD - 1)), MOD)), MOD);
 }
 //-----------------------------------------------------------------------------------------*/
-/*/--------------------------(Trie Data structure)------------------------------------------
+//--------------------------(Trie Data structure)------------------------------------------
 struct node
 {
     ll cnt, nxt[26];
@@ -132,7 +132,7 @@ inline ll qry(string &s)
     return Trie[root].cnt;
 }
 //-----------------------------------------------------------------------------------------*/
-/*/-------------------------(Segment tree with lazy)----------------------------------------
+//-------------------------(Segment tree with lazy)----------------------------------------
 ll a[N], tree[4 * N], lazy[4 * N];
 void init(ll nd, ll l, ll r)
 {
@@ -196,12 +196,18 @@ ll value(ll nd, ll l, ll r, ll &myl, ll &myr)
     return value(nd * 2, l, mid, myl, myr) + value(nd * 2 + 1, mid + 1, r, myl, myr);
 }
 //----------------------------------------------------------------------------------------*/
-/*/-----------------------------(use of sieve)----------------------------------------------
-vector<ll> lp(N, 0), prime(N, 1), sum(N, 0), pod(N, 1), cnt(N, 0);
+//-----------------------------(use of sieve)----------------------------------------------
+vector<ll> lp(N, 0), prime(N, 1), sum(N, 0), pod(N, 1), cnt(N, 0), phi(N);
 void sieve()
 {
+    for (ll i = 1; i < N; i++)
+        phi[i] = i;
     for (ll i = 2; i < N; i++)
     {
+        if (phi[i] == i)
+            for (ll j = i; j < N; j += i)
+                phi[j] -= (phi[j] / i);
+        // phi(a*b) = phi(a) * phi(b) * (gcd(a, b) / (phi{gcd(a, b)}));
         for (ll j = i; j < N; j += i)
         {
             cnt[j]++;
@@ -223,10 +229,38 @@ void sieve()
         }
     }
 }
+//---------------------------------(number theory)------------------------------------
+**snd(p ^ a) = 1 + 2 + 3 + 4 + 5 + -- -- -- -+a + (a + 1);
+**n !er prime factor = (n / p) + (n / p ^ 2) + -- -;
+**(1 - N) porjonto gcd(i, j) > 1 hobe ? ? ? ?
+{
+    total possible pair = (N * (N - 1) / 2);
+    er por ans = total theke sobgulor coprime number bad jabe..**time komate prefix sum;
+    ans = total - pre[N] + (n - 1); // (i==j) er jonno n-1 ta;
+}
+/* minimum number jar divisor sonkha = n
+{
+    ll primearr[]={2,3,5,7,11,13,17,19,23,29,31,37};
+    ll n;
+    ll ans(ll res_now,ll n_now,ll ind)
+    {
+        if(n_now==n)
+        return res_now;
+        ll res=2e8+5;
+        for(ll i=1;i<=70;i++)
+        {
+            res_now*=prime[ind];
+            if(res_now>res||n_now*(i+1)>n)
+            break;
+            res=min(res,ans(res_now,n_now*(i+1),ind+1));
+        }
+        return res;
+    }
+}
 //----------------------------------------------------------------------------------------*/
-/*/------------------------(Dynamic programming)---------------------------------------------
+//------------------------(Dynamic programming)---------------------------------------------
 ll dp[N];
-ll ok(ll rem_sum, vector<ll> &coin)//coin sum
+ll ok(ll rem_sum, vector<ll> &coin) // coin sum
 {
     if (rem_sum == 0)
         return 0;
@@ -241,7 +275,74 @@ ll ok(ll rem_sum, vector<ll> &coin)//coin sum
     }
     rrr dp[rem_sum] = ans;
 }
+**grid e akti rekhar upor koiti bindu ace jegulo purno sonkha
+     ans = gcd((x1 - x2), (y1 - y2)) + 1; // bindu 2 tar katalan distance
 //----------------------------------------------------------------------------------------*/
+//-----------------------------(Graph theory)------------------------------------------------
+vector<ll> g[N], topo;
+ll vis[N], cycle;
+void dfs(ll n) // topological sort
+{
+    vis[n] = 1;
+    for (auto it : g[n])
+    {
+        if (vis[it] == 1)
+            cycle = 1;
+        if (vis[it] == 0)
+            dfs(it);
+    }
+    vis[n] = 2;
+    topo.push_back(n);
+}
+// reverse(all(topo));
+//  LCA
+ll par[N];
+void Dfs(ll v, ll pr = -1)
+{
+    par[v] = pr;
+    for (auto it : g[v])
+    {
+        if (it == pr)
+            continue;
+        Dfs(it, v);
+    }
+}
+vector<ll> path(int v)
+{
+    vector<ll> ans;
+    while (v != -1)
+    {
+        ans.push_back(v);
+        v = par[v];
+    }
+    reverse(all(ans));
+    return ans;
+}
+// abar 2 ta point tehke path ber kore 1st teke jekhane diff pabo oitai ans
+// multisource bfs a sob source gulo age queue te jabe
+//******* DSU ******
+vector<pair<ll, ll>> ans[N];
+ll par[N], sz[N];
+void init(ll a)
+{
+    par[a] = a, sz[a] = 1;
+}
+ll parent(ll a)
+{
+    if (par[a] == a)
+        return a;
+    return par[a] = parent(par[a]);
+}
+void _union(ll a, ll b, ll w)
+{
+    ll pa = parent(a), pb = parent(b);
+    if (sz[pa] < sz[pb])
+        swap(pa, pb);
+    if (pa != pb)
+    {
+        par[pb] = pa, sz[pa] += sz[pb];
+    }
+}
 //----------------------------------------------------------------------------------------*/
 void solve(void)
 {
